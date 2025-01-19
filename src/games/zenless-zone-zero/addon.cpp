b723daba13e1886fdfd4d4876f35389734261b23
Maple <8961085+MapleHinata@users.noreply.github.com>
2025-01-19 19:29:16 -0300

    fix(zenless-zone-zero): fix rgba8 upgrade causing graphical issues

--- src/games/zenless-zone-zero/addon.cpp
+++ src/games/zenless-zone-zero/addon.cpp
@@ -297,6 +297,7 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
       renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
           .old_format = reshade::api::format::r8g8b8a8_typeless,
           .new_format = reshade::api::format::r16g16b16a16_typeless,
+          .index = 0,
           .aspect_ratio = renodx::mods::swapchain::SwapChainUpgradeTarget::ANY
       });
       break;
